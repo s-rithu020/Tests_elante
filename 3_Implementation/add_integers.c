01ef8bf620cec16dd14c92ac4b10488cb57842db
s-rithu020 <rithu165@gmail.com>
2022-03-22 20:37:49 +0530

    initial commit - moving project

---
+++ 3_Implementation/add_integers.c
@@ -0,0 +1,12 @@
+#include "stdio.h"
+int main()
+{
+    int A = 0, B = 0;
+    printf("Enter two numbers to be added\n");
+    scanf("%d %d", &A, &B);
+
+    int sum = A + B;
+    printf("Sum = %d", Sum);
+
+    return 0;
+}
